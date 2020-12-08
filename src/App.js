04d309ec074e9c06dc23c46534743112ea56e286
HizkiaR <hizkiarambitan99@yahoo.com>
2020-12-08 14:32:55 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import {} from 'react-router-dom';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <BrowserRouter>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Covid Tracker</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href=".">Global</Nav.Link>
+              <Nav.Link href="/indonesia">Indonesia</Nav.Link>
+              <Nav.Link href="/provinsi">Provinsi</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </BrowserRouter>
     </div>
   );
 }
